44b92d7bab9bb1c0c274ad02fb536d7934cda1f4
PalSagar30 <sagarpalsp3008@gmail.com>
2023-08-05 12:35:33 +0530

    button flash

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ let userSeq = [];
 
 let started = false;
 let level = 0;
+let buttons = ["red","yellow","green","blue"]
+let h2 = document.querySelector("h2")
+
 
 document.addEventListener("keypress", ()=>{
     if(started === false){
         console.log("started")
         started = true
     }
+
+    levelUp();
 })
+
+function btnFlash(btn){
+    btn.classList.add("flash")
+    setTimeout(()=>{
+        btn.classList.remove("flash")
+    },250)
+}
+
+function levelUp(){
+    level++;
+    h2.innerHTML = `Level ${level}`
+
+    let randInd = Math.floor(Math.random()*3) 
+    let ranColor = buttons[randInd]
+    let randBtn = document.querySelector(`.${ranColor}`)
+    btnFlash(randBtn);
+}
